cd1242732441397300d4b77884b0ee5af15fab1b
manoharchukkala602 <manoharchukkala40@gmail.com>
2026-02-14 08:36:46 +0530

    Update Number guessing game.py

--- Number guessing game.py
+++ Number guessing game.py
@@ -2,15 +2,15 @@ import random
 
 number = random.randint(1, 10)
 
-print("🎯 Guess the number between 1 and 10")
+print(" Guess the number between 1 and 10")
 
 while True:
     guess = int(input("Enter your guess: "))
 
     if guess == number:
-        print("🎉 Correct! You guessed it.")
+        print("Correct! You guessed it.")
         break
     elif guess < number:
-        print("Too low ⬇️")
+        print("Too low ")
     else:
-        print("Too high ⬆️")
+        print("Too high ")
